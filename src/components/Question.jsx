d9cd0d4bc1f9f450de96d04683a24f6ea6b6f284
Mark Linn <marktlinn@gmail.com>
2022-09-21 12:38:18 +0200

    UseEffect for fetch Api updated: ap now outputting buttons for answers, which are shuffled. A cleanup function has been added to the useEffect to stop multiple requests being fired

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -5,18 +5,9 @@ import { TokenContext } from '../context/TokenContext';
 
 export default function Question (){
   const { apiToken } = useContext(TokenContext)
-
-  const [questions, setQuestions] = useState('')
-  const [correctAnswer1, setCorrectAnswer1] = useState('')
-  const [correctAnswer2, setCorrectAnswer2] = useState('')
-  const [correctAnswer3, setCorrectAnswer3] = useState('')
-  const [correctAnswer4, setCorrectAnswer4] = useState('')
-  const [correctAnswer5, setCorrectAnswer5] = useState('')
-  const [answersQ1, setAnswersQ1] = useState('')
-  const [answersQ2, setAnswersQ2] = useState('')
-  const [answersQ3, setAnswersQ3] = useState('')
-  const [answersQ4, setAnswersQ4] = useState('')
-  const [answersQ5, setAnswersQ5] = useState('')
+  const [questions, setQuestions] = useState(null);
+  const [correctAnswers, setCorrectAnswers] = useState(null)
+  const [choices, setChoices] = useState([])
   
   const shuffleArray = (array) => {
     for (let i = array.length - 1; i > 0; i--) {
@@ -30,37 +21,43 @@ export default function Question (){
   
 
   useEffect(()=>{
+    const abortController = new AbortController();
     async function getQuestions(){
       try {
         if(apiToken){
-          const questionsRaw = await fetch(`https://opentdb.com/api.php?amount=5&category=23&difficulty=medium&type=multiple&token=${apiToken}`);
+          const questionsRaw = await fetch(`https://opentdb.com/api.php?amount=5&category=23&difficulty=medium&type=multiple&${apiToken}`, { signal: abortController.signal});
           const data = await questionsRaw.json();
           const results = await data.results;
           setQuestions(results.map(elem=> elem.question));
-          setCorrectAnswer1(results[0].correct_answer);
-          setCorrectAnswer2(results[1].correct_answer);
-          setCorrectAnswer3(results[2].correct_answer);
-          setCorrectAnswer4(results[3].correct_answer);
-          setCorrectAnswer5(results[4].correct_answer);
-          setAnswersQ1(results[0].incorrect_answers)
-          setAnswersQ2(results[1].incorrect_answers)
-          setAnswersQ3(results[2].incorrect_answers)
-          setAnswersQ4(results[3].incorrect_answers)
-          setAnswersQ5(results[4].incorrect_answers)
+          setChoices(results.map(elem=> [...elem.incorrect_answers, elem.correct_answer]))
+          setCorrectAnswers(results.map(elem=> elem.correct_answer))
         }
         
       } catch (error) {
+        if(error.name === 'AbortError'){ 
+          console.log('fetch aborted')
+        }
+        else{
           console.log(`Error with fetching questions: ${error}`)
+        }
       }
     }
     getQuestions()
-  }, [])
+    return ()=> abortController.abort()
+  }, [apiToken])
+
   return (
       <div className='question-card'>
         {questions && <ul>
-            {questions.map(question=> <li>{question}</li>)}
+            {questions.map((question, index)=> {
+            return (
+            <div>
+              <li>{question}</li>
+              {shuffleArray(choices[index]).map((elem)=> <button>{elem}</button>)}
+            </div>
+            )})}
         </ul>}
-
+          <p></p>
         
 
       </div>
