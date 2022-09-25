7a85bc36085860be5d693a13a1c57243e5e3d466
Mark Linn <marktlinn@gmail.com>
2022-09-25 17:34:51 +0200

    added ability to change state of block, need to fix it.

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import { useState} from 'react';
 import { nanoid } from 'nanoid';
-import { Link } from 'react-router-dom';
 
 export default function Quiz({ questions }) {
+  const [questionSelectedBlock, setQuestionSelectedBlock] = useState({0:false, 1:false, 2:false, 3:false});
 
-  // const quest = questions.map(({questions, answers})=> (questions))
-  // const answers = questions.map(({answers})=> answers)
+
+  console.log(questionSelectedBlock)
+  function handleClick(e){
+    const correctAnswer = e.target.getAttribute('correct-answer');
+    const index = +e.target.getAttribute('index')
+    setQuestionSelectedBlock(prevState => ({
+      ...prevState,
+      [index]: !questionSelectedBlock[index]
+    }))
+    console.log(correctAnswer)
+      console.log('index',index,questionSelectedBlock)
+    // console.log(e.target.value === correctAnswer)
+
+  }
     return (
       <div className='question-card'>
         <h3>Today's Questions</h3>
@@ -20,15 +32,18 @@ export default function Quiz({ questions }) {
             <div className='answer-btns'>
             {elem.answers.map(item=> <button 
             key={nanoid()}
+            id={`${item}-${index}`}
+            index={index}
+            correct-answer={elem.correctAnswer}
+            value={item}
+            onClick={handleClick}
             dangerouslySetInnerHTML={{__html: item}}
             />)}
             </div>
+            <br />
             <hr />
           </div>
           ))}
-          <br />
-
-        {/* { questions &&  <div dangerouslySetInnerHTML={{__html: {quest}}}/>} */}
         </ul>
     </div>
   )
