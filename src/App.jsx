f3da22d383ee063f0a54c7568edecae6ab951c38
Mark Linn <marktlinn@gmail.com>
2022-11-14 22:45:05 +0100

    add footer component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 // pages and components
 import Welcome from '../src/pages/Welcome';
 import Quiz from './pages/Quiz';
+import Footer from './components/Footer';
 import blueBlob from './assets/blueBlob.svg';
 import purpleBlob from './assets/purpleBlob.svg';
 
@@ -9,15 +10,16 @@ function App() {
   //setting states
   const [startGame, setStartGame] = useState(false)
   return (
-        <div>
-            {!startGame ? 
-            <Welcome 
-              startGame={startGame}
-              setStartGame={setStartGame}
-            /> :
-            <Quiz/>}
-            <img className='blueBlob' src={blueBlob} alt="floating blob of blue colour" />
-            <img className='purpleBlob' src={purpleBlob} alt="floating blob of purple colour" />
+        <div className='main'>
+              {!startGame ? 
+              <Welcome 
+                startGame={startGame}
+                setStartGame={setStartGame}
+              /> :
+              <Quiz/>}
+              <img className='blueBlob' src={blueBlob} alt="floating blob of blue colour" />
+              <img className='purpleBlob' src={purpleBlob} alt="floating blob of purple colour" />
+          <Footer/>
         </div>
   )
 }
